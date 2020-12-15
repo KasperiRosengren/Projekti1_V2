786026eaf7b364248cad49241c9ae794b957ca25
Tuovinen <68303973+Hitti7@users.noreply.github.com>
2020-12-15 20:38:30 +0200

    Create AccessControl_mysql.ino

---
+++ Esp32_code/AccessControl_mysql/AccessControl_mysql.ino
@@ -0,0 +1,351 @@
+/*
+   --------------------------------------------------------------------------------------------------------------------
+   Example sketch/program showing An Arduino Door Access Control featuring RFID, EEPROM, Relay
+   --------------------------------------------------------------------------------------------------------------------
+   This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+
+   This example showing a complete Door Access Control System
+
+  Simple Work Flow (not limited to) :
+                                     +---------+
+  +----------------------------------->READ TAGS+^------------------------------------------+
+  |                              +--------------------+                                     |
+  |                              |                    |                                     |
+  |                              |                    |                                     |
+  |                         +----v-----+        +-----v----+                                |
+  |                         |MASTER TAG|        |OTHER TAGS|                                |
+  |                         +--+-------+        ++-------------+                            |
+  |                            |                 |             |                            |
+  |                            |                 |             |                            |
+  |                      +-----v---+        +----v----+   +----v------+                     |
+  |         +------------+READ TAGS+---+    |KNOWN TAG|   |UNKNOWN TAG|                     |
+  |         |            +-+-------+   |    +-----------+ +------------------+              |
+  |         |              |           |                |                    |              |
+  |    +----v-----+   +----v----+   +--v--------+     +-v----------+  +------v----+         |
+  |    |MASTER TAG|   |KNOWN TAG|   |UNKNOWN TAG|     |GRANT ACCESS|  |DENY ACCESS|         |
+  |    +----------+   +---+-----+   +-----+-----+     +-----+------+  +-----+-----+         |
+  |                       |               |                 |               |               |
+  |       +----+     +----v------+     +--v---+             |               +--------------->
+  +-------+EXIT|     |DELETE FROM|     |ADD TO|             |                               |
+          +----+     |  EEPROM   |     |EEPROM|             |                               |
+                     +-----------+     +------+             +-------------------------------+
+
+
+   Use a Master Card which is act as Programmer then you can able to choose card holders who will granted access or not
+
+ * **Easy User Interface**
+
+   Just one RFID tag needed whether Delete or Add Tags. You can choose to use Leds for output or Serial LCD module to inform users.
+
+ * **Stores Information on EEPROM**
+
+   Information stored on non volatile Arduino's EEPROM memory to preserve Users' tag and Master Card. No Information lost
+   if power lost. EEPROM has unlimited Read cycle but roughly 100,000 limited Write cycle.
+
+ * **Security**
+   To keep it simple we are going to use Tag's Unique IDs. It's simple and not hacker proof.
+
+   @license Released into the public domain.
+
+   Typical pin layout used:
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   SPI SS      SDA(SS)      10            53        D10        10               10
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+*/
+
+//#include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
+#include <SPI.h>        // RC522 Module uses SPI protocol
+#include <MFRC522.h>  // Library for Mifare RC522 Devices
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <Arduino_JSON.h>
+
+/*
+  Instead of a Relay you may want to use a servo. Servos can lock and unlock door locks too
+  Relay will be used by default
+*/
+
+#include <ESP32Servo.h>
+
+/*
+  For visualizing whats going on hardware we need some leds and to control door lock a relay and a wipe button
+  (or some other hardware) Used common anode led,digitalWriting HIGH turns OFF led Mind that if you are going
+  to use common cathode led or just seperate leds, simply comment out #define COMMON_ANODE,
+*/
+
+//#define COMMON_ANODE
+#ifdef COMMON_ANODE
+#define LED_ON LOW
+#define LED_OFF HIGH
+#else
+#define LED_ON HIGH
+#define LED_OFF LOW
+#endif
+
+#define redLed 13    // Set Led Pins
+#define greenLed 12
+#define blueLed 14
+
+#define wipeB 25    // Button pin for WipeMode
+
+
+// wifi
+const char* ssid = "kassu2.4GHZ";
+const char* password = "omaverkko";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http://192.168.0.101/post-esp-data.php";
+const char* serverName2 = "http://192.168.0.101/esp-data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page.
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+String sensorName = "1";
+String sensorLocation = "Kassun sohva";
+
+Servo myservo;  // create servo object to control a servo
+// 16 servo objects can be created on the ESP32
+
+int pos = 0;    // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
+int servoPin = 33;
+
+
+bool programMode = false;  // initialize programming mode to false
+
+uint8_t successRead;    // Variable integer to keep if we have Successful Read from Reader
+
+byte storedCard[4];   // Stores an ID read from EEPROM
+byte readCard[4];   // Stores scanned ID read from RFID Module
+//byte masterCard[4];   // Stores master card's ID read from EEPROM
+
+// Create MFRC522 instance.
+#define SS_PIN 5
+#define RST_PIN 22
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+char piccUID[32];
+
+///////////////////////////////////////// Setup ///////////////////////////////////
+void setup()
+{
+  //Arduino Pin Configuration
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  //pinMode(wipeB, INPUT_PULLUP);   // Enable pin's pull up resistor
+  //pinMode(relay, OUTPUT);
+  //Be careful how relay circuit behave on while resetting or power-cycling your Arduino
+  //digitalWrite(relay, HIGH);    // Make sure door is locked
+  digitalWrite(redLed, LED_OFF);  // Make sure led is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure led is off
+  digitalWrite(blueLed, LED_OFF); // Make sure led is off
+
+  //Protocol Configuration
+  Serial.begin(115200);  // Initialize serial communications with PC
+  //EEPROM.begin(4096);
+  SPI.begin();           // MFRC522 Hardware uses SPI protocol
+  mfrc522.PCD_Init();    // Initialize MFRC522 Hardware
+
+  Serial.print("connecting to ");
+  Serial.print(ssid);
+
+  //connect to wifi
+  WiFi.begin(ssid, password);
+
+  //wait until connection has been confirmed
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //debugging - output the ip address of the esp
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);    // standard 50 hz servo
+  myservo.attach(servoPin, 500, 2400); // attaches the servo on pin 18 to the servo object
+  // using default min/max of 1000us and 2000us
+  // different servos may require different min/max settings
+  // for an accurate 0 to 180 sweep
+
+  //If you set Antenna Gain to Max it will increase reading distance
+  //mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+
+  Serial.println(F("Access Control Example v0.1"));   // For debugging purposes
+  ShowReaderDetails();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println("Present PICC");
+}
+
+///////////////////////////////////////// Main Loop ///////////////////////////////////
+void loop ()
+{
+  getID(piccUID);
+}
+
+/////////////////////////////////////////  Access Granted    ///////////////////////////////////
+void granted ( uint16_t setDelay) {
+  digitalWrite(blueLed, LED_OFF);   // Turn off blue LED
+  digitalWrite(redLed, LED_OFF);  // Turn off red LED
+  digitalWrite(greenLed, LED_ON);   // Turn on green LED
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);    // tell servo to go to position in variable 'pos'
+    delay(15);             // waits 15ms for the servo to reach the position
+  }
+  delay(setDelay);          // Hold door lock open for given seconds
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);    // tell servo to go to position in variable 'pos'
+    delay(15);             // waits 15ms for the servo to reach the position
+  }
+  delay(1000);            // Hold green LED on for a second
+}
+
+///////////////////////////////////////// Access Denied  ///////////////////////////////////
+void denied() {
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  digitalWrite(redLed, LED_ON);   // Turn on red LED
+  delay(1000);
+}
+
+
+///////////////////////////////////////// Get PICC's UID ///////////////////////////////////
+char getID(char uid[32])
+{
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return 1;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return 1;
+  }
+
+  array_to_string(mfrc522.uid.uidByte, 4, uid); //Insert (byte array, length, char array for output)
+  Serial.println(uid); //Print the output uid string
+  mfrc522.PICC_HaltA();
+  SendData(uid);
+}
+
+////////////////////Turn Uid into char string///////////////////
+void array_to_string(byte array[], unsigned int len, char buffer[])
+{
+  for (unsigned int i = 0; i < len; i++)
+  {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i * 2 + 0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+    buffer[i * 2 + 1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+  }
+  buffer[len * 2] = '\0';
+}
+
+void SendData(char UID[32])
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http; Serial;
+
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Prepare your HTTP POST request data
+    //int temperature = lueDHTlampo();
+    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                             + "&location=" + sensorLocation + "&value1=" + UID;
+
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+
+    if (httpResponseCode > 0)
+    {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else
+    {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else
+  {
+    Serial.println("WiFi Disconnected");
+  }
+}
+
+void ShowReaderDetails()
+{
+  // Get the MFRC522 software version
+  byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+  Serial.print(F("MFRC522 Software Version: 0x"));
+  Serial.print(v, HEX);
+  if (v == 0x91)
+    Serial.print(F(" = v1.0"));
+  else if (v == 0x92)
+    Serial.print(F(" = v2.0"));
+  else
+    Serial.print(F(" (unknown),probably a chinese clone?"));
+  Serial.println("");
+  // When 0x00 or 0xFF is returned, communication probably failed
+  if ((v == 0x00) || (v == 0xFF)) {
+    Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
+    Serial.println(F("SYSTEM HALTED: Check connections."));
+    // Visualize system is halted
+    digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+    digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+    digitalWrite(redLed, LED_ON);   // Turn on red LED
+    while (true); // do not go further
+  }
+}
+
+///////////////////////////////////////// Cycle Leds (Program Mode) ///////////////////////////////////
+void cycleLeds()
+{
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_ON);   // Make sure green LED is on
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  delay(200);
+  digitalWrite(redLed, LED_OFF);  // Make sure red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  digitalWrite(blueLed, LED_ON);  // Make sure blue LED is on
+  delay(200);
+  digitalWrite(redLed, LED_ON);   // Make sure red LED is on
+  digitalWrite(greenLed, LED_OFF);  // Make sure green LED is off
+  digitalWrite(blueLed, LED_OFF);   // Make sure blue LED is off
+  delay(200);
+}
+
+//////////////////////////////////////// Normal Mode Led  ///////////////////////////////////
+void normalModeOn ()
+{
+  digitalWrite(blueLed, LED_ON);  // Blue LED ON and ready to read card
+  digitalWrite(redLed, LED_OFF);  // Make sure Red LED is off
+  digitalWrite(greenLed, LED_OFF);  // Make sure Green LED is off
+  //digitalWrite(relay, HIGH);    // Make sure Door is Locked
+}
